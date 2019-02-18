f2acc3e23a7bb29ef3e02de9bda1f151c20b7b59
Quentin Rousseau <contact@quent.in>
2019-02-18 10:58:51 -0800

    SSR

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,13 @@ const path = require('path');
 
 module.exports = {
   entry: './src/js/react-timeslot-calendar.jsx',
+  externals: ['react', 'react-dom'],
+  target: 'node',
 
   output: {
     path: path.join(__dirname, './build'),
     filename: 'build.min.js',
-    libraryTarget: 'umd',
+    libraryTarget: 'commonjs2',
   },
   plugins: [
     new WebpackBundleSizeAnalyzerPlugin('./reports/plain-report.txt'),
